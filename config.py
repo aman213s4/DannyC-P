92417f5f49a7096c8e9c655f8b10d8a6d285ccff
aman213s4 <113830087+aman213s4@users.noreply.github.com>
2024-06-13 07:00:42 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6309748482:AAHtCMyL-EoYqXar0rptpjWTlRS0QFzFG-Q")
+    API_ID = int(os.environ["API_ID", 24643568]
+    API_HASH = os.environ["API_HASH", "5c3ea512a2730bfab77e3d75f6272e7b"]
+    AUTH_USERS = "7175040177"""
+
